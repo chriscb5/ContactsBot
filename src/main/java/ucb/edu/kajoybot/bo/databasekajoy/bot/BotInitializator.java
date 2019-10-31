a7dd7b6730c51b100e8e7f931a2e79c7b623b040
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-10-31 15:42:43 -0400

    Add contructors in  BotInitializator class

--- src/main/java/ucb/edu/kajoybot/bo/databasekajoy/bot/BotInitializator.java
+++ src/main/java/ucb/edu/kajoybot/bo/databasekajoy/bot/BotInitializator.java
@@ -1,9 +1,22 @@
 package ucb.edu.kajoybot.bo.databasekajoy.bot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
+import ucb.edu.kajoybot.bo.databasekajoy.dao.EstudianteRespository;
 
 @Component
 public class BotInitializator {
+
+    EstudianteRespository estudianteRespository;
+
+    @Autowired
+    public BotInitializator(EstudianteRespository estudianteRespository) {
+        this.estudianteRespository = estudianteRespository;
+    }
+
+    public BotInitializator() {
+    }
+
+
 
 }
