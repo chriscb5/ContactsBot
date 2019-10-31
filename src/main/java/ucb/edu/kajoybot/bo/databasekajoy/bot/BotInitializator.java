11581d9f8a6aeafbe7534f13038b7c193ae3b98c
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-10-31 15:43:16 -0400

    Add init function  in  BotInitializator class

--- src/main/java/ucb/edu/kajoybot/bo/databasekajoy/bot/BotInitializator.java
+++ src/main/java/ucb/edu/kajoybot/bo/databasekajoy/bot/BotInitializator.java
@@ -2,7 +2,12 @@ package ucb.edu.kajoybot.bo.databasekajoy.bot;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ucb.edu.kajoybot.bo.databasekajoy.dao.EstudianteRespository;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class BotInitializator {
@@ -17,6 +22,15 @@ public class BotInitializator {
     public BotInitializator() {
     }
 
-
+    @PostConstruct
+    public void init() {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new MainBot(estudianteRespository));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
