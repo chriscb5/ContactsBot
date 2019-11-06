e56824fb5a3676be63a231d872eb91e93aa8839f
Celis <mau.armata@gmail.com>
2019-11-06 17:45:43 -0400

    Realizando cambios en la clase Estudiante Controller

--- src/main/java/ucb/edu/kajoybot/bo/databasekajoy/api/EstudianteController.java
+++ src/main/java/ucb/edu/kajoybot/bo/databasekajoy/api/EstudianteController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ucb.edu.kajoybot.bo.databasekajoy.bl.PersonBL;
 import ucb.edu.kajoybot.bo.databasekajoy.dao.EstudianteRespository;
 import ucb.edu.kajoybot.bo.databasekajoy.domain.EstudianteEntity;
 import ucb.edu.kajoybot.bo.databasekajoy.dto.EstudianteDto;
@@ -15,31 +17,41 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/estudiante")
-public class EstudianteController {
+public class   EstudianteController {
 
-    private EstudianteRespository estudianteRespository;
+    //    private EstudianteRespository estudianteRespository;
+    private PersonBL personBL;
 
 
     @Autowired
-    public EstudianteController(EstudianteRespository estudianteRespository){
-        this.estudianteRespository = estudianteRespository;
+    public EstudianteController(PersonBL personBL) {
+        this.personBL = personBL;
 
     }
 
-    @RequestMapping(value = "/",method = RequestMethod.GET,
-    produces = MediaType.APPLICATION_JSON_VALUE)
+    //    @RequestMapping(value = "/",method = RequestMethod.GET,
+    //   produces = MediaType.APPLICATION_JSON_VALUE)
   /*  List<EstudianteEntity> all(){
         return estudianteRespository.findAll();
 
     }*/
-  List<EstudianteDto> all()
-  {
-      List<EstudianteDto> estudianteDtoList=new ArrayList<>();
+    //List<EstudianteDto> all()
+    //{
+/*      List<EstudianteDto> estudianteDtoList=new ArrayList<>();
       for (EstudianteEntity estudianteEntity:estudianteRespository.findAll()){
           estudianteDtoList.add(new EstudianteDto(estudianteEntity));
       }
-      return estudianteDtoList;
+*/  //    return personBL.findAllEstudiantes();
+    //}
+    @RequestMapping(value = "/", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    List<EstudianteDto> all(@RequestParam(name = "includeCursos") boolean includeCursos) {
+        if (includeCursos) {
+            return personBL.findAllEstudiantesWithCursos();
+        } else {
+            return personBL.findAllEstudiantesWithCursos();
+        }
 
-  }
 
+    }
 }
