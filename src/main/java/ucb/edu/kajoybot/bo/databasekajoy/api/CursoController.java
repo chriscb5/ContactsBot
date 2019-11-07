155b3c986df5c9f905989270bee6c4352b78ce0c
Christoffer Calderón de la Barca <chris.c.b.5.b@gmail.com>
2019-11-07 18:13:16 -0400

    Added Crear Nuevo Curso

--- src/main/java/ucb/edu/kajoybot/bo/databasekajoy/api/CursoController.java
+++ src/main/java/ucb/edu/kajoybot/bo/databasekajoy/api/CursoController.java
@@ -5,6 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import ucb.edu.kajoybot.bo.databasekajoy.bl.CursoBL;
 import ucb.edu.kajoybot.bo.databasekajoy.dao.CursoRepository;
 import ucb.edu.kajoybot.bo.databasekajoy.domain.CursoEntity;
 import ucb.edu.kajoybot.bo.databasekajoy.dto.CursoDto;
@@ -16,20 +17,20 @@ import java.util.List;
 @RequestMapping("/k1/curso")
 public class CursoController {
 
-    private CursoRepository cursoRepository;
+    private CursoBL cursoBL;
 
     @Autowired
-    public CursoController(CursoRepository cursoRepository){
-        this.cursoRepository = cursoRepository;
+    public CursoController(CursoBL cursoBL){
+        this.cursoBL = cursoBL;
     }
 
     @RequestMapping(value = "/",method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
     List<CursoDto> all(){
         List<CursoDto> cursoDtoList = new ArrayList<>();
-        for (CursoEntity cursoEntity:cursoRepository.findAll()){
-            cursoDtoList.add(new CursoDto(cursoEntity));
-        }
+        //for (CursoEntity cursoEntity:cursoBL.findAll()){
+            cursoDtoList.add(new CursoDto(cursoBL.findCursoById(1)));
+        //}
         return cursoDtoList;
     }
 
