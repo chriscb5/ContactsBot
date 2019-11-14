e865ecb741434109698870c0c138e1196c0c54a2
Celis <mau.armata@gmail.com>
2019-11-14 17:58:21 -0400

    Agregando nuevo metodo a docente repository

--- src/main/java/ucb/edu/kajoybot/bo/databasekajoy/dao/DocenteRespository.java
+++ src/main/java/ucb/edu/kajoybot/bo/databasekajoy/dao/DocenteRespository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface DocenteRespository extends JpaRepository<DocenteEntity, Integer> {
     List<DocenteEntity> findAllByStatuss(int status);
+    DocenteEntity findAllByNombre(String nombre);
 }
