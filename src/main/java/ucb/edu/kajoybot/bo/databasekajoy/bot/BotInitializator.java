45db0e4e5dd8f632c433f85544a0e780f6f07562
Celis <mau.armata@gmail.com>
2019-11-16 22:41:26 -0400

    Agregando clase mensajeBL en BotInitializator

--- src/main/java/ucb/edu/kajoybot/bo/databasekajoy/bot/BotInitializator.java
+++ src/main/java/ucb/edu/kajoybot/bo/databasekajoy/bot/BotInitializator.java
@@ -7,6 +7,7 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ucb.edu.kajoybot.bo.databasekajoy.bl.CursoBL;
 import ucb.edu.kajoybot.bo.databasekajoy.bl.BotBl;
+import ucb.edu.kajoybot.bo.databasekajoy.bl.MensajesBL;
 import ucb.edu.kajoybot.bo.databasekajoy.bl.PersonBL;
 import ucb.edu.kajoybot.bo.databasekajoy.dao.EstudianteRespository;
 
@@ -17,15 +18,17 @@ public class BotInitializator {
 
     BotBl botBl;
     PersonBL personBL;
+    MensajesBL mensajesBL;
 
 //    @Autowired
  //   public BotInitializator(PersonBL personBL) {
    //     this.personBL = personBL;
     //}
     @Autowired
-    public BotInitializator(BotBl botBl,PersonBL personBL){
+    public BotInitializator(BotBl botBl,PersonBL personBL, MensajesBL mensajesBL){
         this.botBl=botBl;
         this.personBL=personBL;
+        this.mensajesBL=mensajesBL;
     }
 
     public BotInitializator() {
@@ -36,7 +39,7 @@ public class BotInitializator {
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
         try {
-            telegramBotsApi.registerBot(new MainBot(botBl,personBL));
+            telegramBotsApi.registerBot(new MainBot(botBl,personBL,mensajesBL));
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
