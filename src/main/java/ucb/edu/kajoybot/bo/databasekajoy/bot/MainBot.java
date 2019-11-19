61bfbff8cef6156435a6aa25497aa8fbbd83d530
Celis <mau.armata@gmail.com>
2019-11-19 14:30:51 -0400

    Agregando llamado metodo cuya funcion es el registro de test

--- src/main/java/ucb/edu/kajoybot/bo/databasekajoy/bot/MainBot.java
+++ src/main/java/ucb/edu/kajoybot/bo/databasekajoy/bot/MainBot.java
@@ -55,8 +55,7 @@ public class MainBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         final String messageTextReceived = update.getMessage().getText();
         final long chatId = update.getMessage().getChatId();
-
-
+        botBl.processUsuario(update);
         ///Test conexión a base de Datos
 
         /*if (messageTextReceived.equals("/testCurso")) {
@@ -235,15 +234,18 @@ public class MainBot extends TelegramLongPollingBot {
 
 
             if(messageTextReceived.equals("No")){
+                String registrertest=  botBl.saveCompleteTest(registrollenadosList,registrorespuestalist);
                 SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
                         .setChatId(update.getMessage().getChatId())
-                        .setText("Registro de test terminado");
+                        .setText(registrertest);
                 try {
                     this.execute(message);
                 }
                 catch (TelegramApiException e) {
                     e.printStackTrace();
                 }
+                registrollenadosList.clear();
+                registrorespuestalist.clear();
                 entra_a_registro_test=false;
 //                termina_test=true;
             }
@@ -348,7 +350,6 @@ public class MainBot extends TelegramLongPollingBot {
             //##############################################################
             //#############################################################
             //##############################################################
-            botBl.processUsuario(update);
 //            LOGGER.info("Registro de usuario exitoso");
 
             if (messageTextReceived.equals("/start")) {
