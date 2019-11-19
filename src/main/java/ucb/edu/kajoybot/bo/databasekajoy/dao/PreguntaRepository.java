5b83bd82ea36a2d2290decaff07cf72b86014152
Celis <mau.armata@gmail.com>
2019-11-19 14:22:51 -0400

    Adicionando interface Preguntarepository y los metodos

---
+++ src/main/java/ucb/edu/kajoybot/bo/databasekajoy/dao/PreguntaRepository.java
@@ -0,0 +1,14 @@
+package ucb.edu.kajoybot.bo.databasekajoy.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ucb.edu.kajoybot.bo.databasekajoy.domain.PreguntaEntity;
+import ucb.edu.kajoybot.bo.databasekajoy.domain.TestEntity;
+
+import java.util.List;
+
+public interface PreguntaRepository extends JpaRepository<PreguntaEntity,Integer> {
+
+    List<PreguntaEntity> findAllByIdPregunta(int idPregunta);
+    List<PreguntaEntity> findAllByIdTest(int idTest);
+    PreguntaEntity findAllByContenidoPreguntaAndIdTest(String content_question, TestEntity entity);
+}
