1f1dc4c10d0cd877651e6dda61b8de19be309af8
JohnnHidalgo <31965496+JohnnHidalgo@users.noreply.github.com>
2019-11-22 18:24:08 -0400

    add query in ChatRepository

--- src/main/java/ucb/edu/kajoybot/bo/databasekajoy/dao/ChatRepository.java
+++ src/main/java/ucb/edu/kajoybot/bo/databasekajoy/dao/ChatRepository.java
@@ -1,4 +1,11 @@
 package ucb.edu.kajoybot.bo.databasekajoy.dao;
 
-public interface ChatRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import ucb.edu.kajoybot.bo.databasekajoy.domain.KjChatEntity;
+
+public interface ChatRepository extends JpaRepository<KjChatEntity,Integer> {
+    @Query(value = "SELECT * FROM kj_chat where kjuserid =?1 ORDER BY kjchat_id ",nativeQuery =true)
+    public KjChatEntity findLastChatByUserId(Integer userId);
 }
+
