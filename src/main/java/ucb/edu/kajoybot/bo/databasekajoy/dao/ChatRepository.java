719f9f94d688ecd81e380300dcd16a11317bc84c
Celis <mau.armata@gmail.com>
2019-11-22 21:43:24 -0400

    Realizando arreglos en query correspondiente a la tabla kj_chat

--- src/main/java/ucb/edu/kajoybot/bo/databasekajoy/dao/ChatRepository.java
+++ src/main/java/ucb/edu/kajoybot/bo/databasekajoy/dao/ChatRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import ucb.edu.kajoybot.bo.databasekajoy.domain.KjChatEntity;
 
 public interface ChatRepository extends JpaRepository<KjChatEntity,Integer> {
-    @Query(value = "SELECT * FROM kj_chat where kjuserid =?1 ORDER BY kjchat_id ",nativeQuery =true)
+    @Query(value = "SELECT * FROM kj_chat where kjuserid =?1 ORDER BY kjchat_id DESC LIMIT 1",nativeQuery =true)
     public KjChatEntity findLastChatByUserId(Integer userId);
 }
 
